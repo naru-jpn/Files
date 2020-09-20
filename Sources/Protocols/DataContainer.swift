ec435c5d956a4357f0f74a4b0671d7aae0da7272
naru-jpn <tus.naru@gmail.com>
2020-09-20 15:54:11 +0900

    Add to get data

--- Sources/Protocols/DataContainer.swift
+++ Sources/Protocols/DataContainer.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 public protocol DataContainer: Locatable {
+
+    /// Contents of this file.
+    var data: Data? { get }
 }
 
 extension DataContainer {
+
+    public var data: Data? {
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            return nil
+        }
+    }
 }
